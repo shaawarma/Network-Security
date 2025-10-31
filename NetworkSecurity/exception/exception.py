ed43bf6e861a32bce3d68cb40f0ce344c5ff5341
shaawarma <varunvassagudam@gmail.com>
2025-11-01 03:45:09 +0530

    completed training, evaluation and mlflow

--- NetworkSecurity/exception/exception.py
+++ NetworkSecurity/exception/exception.py
@@ -1,21 +1,33 @@
 import sys
+from typing import Any
 from NetworkSecurity.my_logging import logger
+
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
+    def __init__(self, error_message: Exception, error_details: Any):
+        super().__init__(error_message)
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
         
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename 
-    
+        exc_type, exc_value, exc_tb = sys.exc_info()
+
+        if exc_tb is not None:
+            self.lineno = exc_tb.tb_lineno
+            self.file_name = exc_tb.tb_frame.f_code.co_filename
+        else:
+            # fallback in case traceback not available
+            self.lineno = "Unavailable"
+            self.file_name = "Unknown File"
+
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-        
-if __name__=='__main__':
+        return (
+            f"Error occurred in python script [{self.file_name}] "
+            f"at line [{self.lineno}] "
+            f"with message: {self.error_message}"
+        )
+
+
+if __name__ == "__main__":
     try:
-        logger.logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed",a)
+        logger.logging.info("Testing NetworkSecurityException class...")
+        a = 1 / 0
     except Exception as e:
-           raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
 from NetworkSecurity.components.data_validation import DataValidation
 from NetworkSecurity.components.data_transformation import DataTransformation
+from NetworkSecurity.components.model_trainer import ModelTrainer
 from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.my_logging.logger import logging
 from NetworkSecurity.entity.config_entity import DataIngestionConfig
 from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
 from NetworkSecurity.entity.config_entity import DataValidationConfig
 from NetworkSecurity.entity.config_entity import DataTransformationConfig
+from NetworkSecurity.entity.config_entity import ModelTrainerConfig
 
 import sys
 
@@ -31,6 +33,13 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+        
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
         
         
         
