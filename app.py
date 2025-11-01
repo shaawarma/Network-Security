ed92a5fa0f34ec6e39df4683ba180d5e1818c608
shaawarma <varunvassagudam@gmail.com>
2025-11-02 01:23:08 +0530

    batch prediction added

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
 from NetworkSecurity.utils.main_utils.utils import load_object
+from NetworkSecurity.utils.ml_utils.model.estimator import NetworkModel
 
 username = quote_plus(os.getenv("MONGO_USERNAME", ""))
 password = quote_plus(os.getenv("MONGO_PASSWORD", ""))
@@ -34,6 +35,9 @@ from NetworkSecurity.constant.training_pipeline import DATA_INGESTION_DATABASE_N
 
 database = client[DATA_INGESTION_DATABASE_NAME]
 collection = database[DATA_INGESTION_COLLECTION_NAME]
+
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
 
 app = FastAPI()
 origins = ["*"]
@@ -57,6 +61,30 @@ async def train_route():
         return Response("Training is successful")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+
+
+@app.post("/predict")
+async def predict_route(request: Request,file: UploadFile = File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        #print(df)
+        preprocesor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
     
 if __name__ == "__main__":
     app_run(app,host = "localhost",port = 8000)
